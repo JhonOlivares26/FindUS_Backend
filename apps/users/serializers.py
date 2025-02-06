5bafbece577ac9aa9911b0595d337f81fc2b4791
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2025-02-05 19:22:23 -0500

    Users serializer update

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,26 +1,85 @@
-# users/serializers.py
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User
+from .utils.gen_words import generate_random_username
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False)
+
     class Meta:
-        model = get_user_model()
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        model = User
+        fields = ["id", "email", "name", "last_name", "phone", "password", "user_type", "username"]
+        extra_kwargs = {"password": {"write_only": True}, "user_type": {"read_only": True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        if not user.username:
+            user.username = generate_random_username()
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+class RecoverPasswordSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    re_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "password", "re_password"]
 
     def validate(self, attrs):
-        user = authenticate(username=attrs['username'], password=attrs['password'])
-        if user is None:
-            raise serializers.ValidationError("Invalid credentials")
+        if attrs["password"] != attrs["re_password"]:
+            raise serializers.ValidationError("Passwords do not match")
+        return attrs
 
-        refresh = RefreshToken.for_user(user)
-        return {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
+    def update(self, instance, validated_data):
+        del validated_data["re_password"]
+        instance.set_password(validated_data["password"])
+        instance.save()
+        return instance
+
+
+class UpdateProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "name", "last_name", "phone"]
+
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    user_type = serializers.CharField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+    phone = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        email_or_username: str = data.get("email").lower()
+        password: str = data.get("password")
+
+        if email_or_username and password:
+            try:
+                validate_email(email_or_username)
+                user: User = User.objects.filter(email=email_or_username).first()
+            except ValidationError:
+                user: User = User.objects.filter(username=email_or_username).first()
+
+            if user and user.check_password(password):
+                data["user"] = user
+                return data
+
+            raise serializers.ValidationError("Invalid Credentials")
+
+        raise serializers.ValidationError("Credentials are required")
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "name", "last_name", "phone", "password", "user_type", "username"]
+        extra_kwargs = {"id": {"read_only": True}}
